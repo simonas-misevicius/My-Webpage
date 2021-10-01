5d1f56bc9b76a3c00cd2bb4c1e19242ff1613e46
simonas-misevicius <85928171+simonas-misevicius@users.noreply.github.com>
2021-10-01 09:53:18 +0300

    switch to create-react-app project

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import MakeInput from './components/MakeInput';
-import { BrowserRouter as Router } from 'react-router-dom';
-  
-render(<Router>
-    <MakeInput/>
-  </Router>,
-  document.getElementById('root'));
