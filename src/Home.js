c286c5dace11f70394ebfa617dee198abedb2fe4
simonas-misevicius <writeln.simon@gmail.com>
2021-10-28 10:10:36 +0300

    try use axios to call web service

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
 
-class Home extends React.Component {
-  render() {
-    return (
-      <Container>
-        <Row>
-          <h2 class="text-center">
-            Ongoing project to show my web programming skills.
-          </h2>
-        </Row>
-        <br />
-        <Row>
-          <h2 class="text-center ">
-            Click links at the top of the page to see different small programs.
-          </h2>
-        </Row>
-      </Container>
-    );
-  }
+export default function Home() {
+  const axios = require("axios");
+  axios
+    .get("https://simonasrest.azurewebsites.net/api/Todo")
+    .then(function (response) {
+      // handle success
+      console.log(response.data[0].name);
+    });
+  return (
+    <Container>
+      <Row>
+        <h2 className="text-center">
+          Ongoing project to show my web programming skills.
+        </h2>
+      </Row>
+      <br />
+      <Row>
+        <h2 className="text-center ">
+          Click links at the top of the page to see different small programs.
+        </h2>
+      </Row>
+    </Container>
+  );
 }
-
-export default Home;
